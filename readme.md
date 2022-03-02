ccf39a26907436d325a6f29eeadaa28548ed4dd6
Tom Warsop <68218278+twarsop@users.noreply.github.com>
2022-03-02 09:22:19 +0000

    Adding tutorial 4 - db with Flask-SQLAlchemy

--- readme.md
+++ readme.md
@@ -7,3 +7,84 @@ I'm using this repo as a place to store my code as I work through the Corey Scha
 - Part 1 - Getting Started: https://www.youtube.com/watch?v=MwZwr5Tvyxo
 - Part 2 - Templates: https://www.youtube.com/watch?v=QnDWIZuWYW0
 - Part 3 - Forms and User Input: https://www.youtube.com/watch?v=UIJKdCIEXUQ
+- Part 4 - Database with Flask-SQLAlchemy: https://www.youtube.com/watch?v=cYWiDiIUxQc
+
+## Testing the DB in Part 4
+
+Adding data in `python` repl:
+
+```
+from flask_app import db
+db.create_all()
+from flask_app import User, Post
+user_1 = User(username='Corey', email='C@demo.com', password='password')
+db.session.add(user_1)
+user_2 = User(username='JohnDoe', email='jd@demo.com', password='password')
+db.session.add(user_2)
+db.session.commit()
+```
+
+```
+user = User.query.get(1)
+post_1 = Post(title='Blog 1', content='First Post Content!', user_id=user.id)
+post_2 = Post(title='Blog 2', content='Second Post Content!', user_id=user.id)
+db.session.add(post_1)
+db.session.add(post_2)
+db.session.commit()
+```
+
+Querying the seeded data (again in `python` repl):
+
+```
+User.query.all()
+-> [User('Corey', 'C@demo.com', 'default.jpg'), User('JohnDoe', 'jd@demo.com', 'default.jpg')]
+```
+
+```
+User.query.first()
+-> User('Corey', 'C@demo.com', 'default.jpg')
+```
+
+```
+User.query.filter_by(username='Corey').all()
+-> [User('Corey', 'C@demo.com', 'default.jpg')]
+```
+
+```
+User.query.filter_by(username='Corey').first()
+-> User('Corey', 'C@demo.com', 'default.jpg')
+```
+
+```
+user = User.query.filter_by(username='Corey').first()
+user
+-> User('Corey', 'C@demo.com', 'default.jpg')
+
+user.id
+-> 1
+```
+
+```
+user = User.query.get(1)
+user
+-> User('Corey', 'C@demo.com', 'default.jpg')
+
+user.posts    # before seeding the post data (above)
+-> []     
+
+user.posts    # after seeding the post data (above)
+-> [Post('Blog 1', '2022-03-02 09:13:37.712607'), Post('Blog 2', '2022-03-02 09:13:37.719281')]
+```
+
+```
+post = Post.query.first()
+post.author
+-> User('Corey', 'C@demo.com', 'default.jpg')
+```
+
+Tidying up, dropping the test data and re-creating the tables:
+
+```
+db.drop_all()
+db.create_all()
+```
--- src/flask_app.py
+++ src/flask_app.py
@@ -1,9 +1,37 @@
+from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 
-app = Flask(__name__)
 
+app = Flask(__name__)
 app.config['SECRET_KEY'] = 'faed40ab60bdb30534f4890cb149d64b'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+
 
 posts = [
     {
@@ -20,14 +48,17 @@ posts = [
     }
 ]
 
+
 @app.route('/')
 @app.route('/home')
 def home():
     return render_template('home.html', posts=posts)
 
+
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -36,6 +67,7 @@ def register():
         flash(f'Account created for {form.username.data}!', 'success')
         return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -47,6 +79,7 @@ def login():
         else:
             flash('Login unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
